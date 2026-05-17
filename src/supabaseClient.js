c2f3df8270d48ed209fa0ac2883fa2ffc23f69de
braden129 <bradenfortunato@gmail.com>
2026-05-17 10:09:00 -0400

    supabase auth working

---
+++ src/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
